c0eb826df616bcddc7bc1b78f5bc00368cab1288
AysegulAkcan <gulayseakcan63@gmail.com>
2025-12-29 21:34:27 +0300

    .

--- AppointmentManagement.API/Program.cs
+++ AppointmentManagement.API/Program.cs
@@ -4,49 +4,38 @@ using AppointmentManagement.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:4200",  // Local development
+                "https://appointment-management-k7p9.vercel.app"  // Production
+              )
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();  // Eðer cookie/auth kullanýyorsanýz
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAngular",
-        policy =>
-        {
-            policy
-                .WithOrigins(
-                    "https://appointment-management-k7p9.vercel.app"
-                )
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
-
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
 if (!app.Environment.IsEnvironment("Test"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
 
-app.UseAuthorization();
 
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
